db46dd5920a71e4e6395f5da8fa6d8a92d7abd81
Chell-Minn <151285@csgaugustinus.nl>
2019-12-03 10:56:56 +0000

    JEYS

--- JS/test.js
+++ JS/test.js
@@ -1,14 +1,22 @@
+var diameter = 0
+var hoogte = 0
+
 function setup() {
   canvas = createCanvas(450,450);
-  background('silver');
   canvas.parent('processing');
-  //noLoop();
+  frameRate (100);
 }
 
 function draw() {
+  background('lightgrey');
   noStroke();
-  fill('steelblue');
-  ellipse(450,450,200);
   fill('deepskyblue');
-  ellipse(100,100,100);
+  ellipse(450,450,diameter);
+  fill('silver')
+  ellipse(0,0,diameter)
+  fill('black');
+  ellipse(100,hoogte,100);
+  hoogte = constrain(hoogte,100,height-50);
+  hoogte++; 
+  diameter++;
 }

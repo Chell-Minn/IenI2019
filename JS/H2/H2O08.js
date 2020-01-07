767d4ed65a20bec1cdd2c7a4998a43ac4e94aeba
Chell-Minn <151285@csgaugustinus.nl>
2020-01-07 11:05:27 +0000

    HSUI

--- JS/H2/H2O08.js
+++ JS/H2/H2O08.js
@@ -14,13 +14,15 @@ function setup() {
 function draw() {
   tekenAchtergrond();
 
-  
-    tekenHuis(kleur,13);
+  for (var h = 0;h < huisNummers.length;h++) {
+    tekenHuis(kleur,huisNummers[h],huisEigenaren[h]);
     translate(150,0);
+  }
+    
 
 }
 
-function tekenHuis(kleur,nr) {
+function tekenHuis(kleur,nr,naam) {
   push();
   fill(kleur);
   rect(0,110,150,150);
@@ -32,7 +34,7 @@ function tekenHuis(kleur,nr) {
   text(nr,75,190);
   fill('black');
   textSize(15);
-  text("naam",20,165);
+  text(naam,20,165);
   pop();
 }
 
